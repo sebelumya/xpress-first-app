abcd1daa7037a4918a71af0e3f0000a576c359cd
sebelumya <79953569+sebelumya@users.noreply.github.com>
2021-05-10 01:20:35 +0700

    deleted unnecessary files since now we're only using ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
